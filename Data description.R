17ca11e2b5355d48b7114ebb6a7f0e0bc80cf8f9
RasmuSkovOlesen <rso@ign.ku.dk>
2020-07-22 11:54:26 +0200

    Data description

---
+++ Data description.R
@@ -0,0 +1,37 @@
+#add collaborator
+git config --global user.name RasmuSkovOlesen 
+git config --global user.email rso@ign.ku.dk
+
+
+#libraries
+install.packages("reshape2")
+library(reshape2)
+library(dplyr)
+install.packages("openxlsx")
+library(openxlsx)
+library(tidyverse)
+
+#Histogram over forest cover
+qplot(wave3data$forest.ha,
+      geom="histogram",
+      main = "Forest cover", 
+      xlab = "Ha", 
+      fill=I("dark blue"),
+      col=I("green"))
+
+#Histogram over Dietary diversity score
+qplot(wave3data$mhdds9,
+      geom="histogram",
+      main = "Dietary Diversity Score", 
+      xlab = "DDS value", 
+      fill=I("dark blue"),
+      col=I("green"))
+
+#Histogram over Dietary diversity score
+##Doesn't work :(
+qplot(wave3data$wealth.score,
+      geom="histogram",
+      main = "Wealth of respondents", 
+      xlab = "Wealth Score (1-5)", 
+      fill=I("dark blue"),
+      col=I("green"))
