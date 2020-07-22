37d1033136cf20cad581fd790c3384ed86506aef
RasmuSkovOlesen <rso@ign.ku.dk>
2020-07-22 12:35:52 +0200

    update

--- Data description.R
+++ Data description.R
@@ -11,7 +11,8 @@ install.packages("openxlsx")
 library(openxlsx)
 library(tidyverse)
 
-#Histogram over forest cover
+####HISTOGRAMS####
+##Histogram over forest cover
 qplot(wave3data$forest.ha,
       geom="histogram",
       main = "Forest cover", 
@@ -19,7 +20,7 @@ qplot(wave3data$forest.ha,
       fill=I("dark blue"),
       col=I("green"))
 
-#Histogram over Dietary diversity score
+##Histogram over Dietary diversity score
 qplot(wave3data$mhdds9,
       geom="histogram",
       main = "Dietary Diversity Score", 
@@ -27,10 +28,52 @@ qplot(wave3data$mhdds9,
       fill=I("dark blue"),
       col=I("green"))
 
-#Histogram over Dietary diversity score
+##Histogram over Dietary diversity score
 qplot(wave3data$wealth.score,
       geom="histogram",
       main = "Wealth of respondents", 
       xlab = "Wealth Score (1-5)", 
       fill=I("dark blue"),
       col=I("green"))
+
+##Histogram over Forest Patches
+qplot(wave3data$forest.patches,
+      geom="histogram",
+      main = "Forest Patches", 
+      xlab = "Number of forest patches", 
+      fill=I("dark blue"),
+      col=I("green"))
+
+##Histogram over Education
+wave3data$education <- as.factor(wave3data$education)
+#doesn't work
+qplot(wave3data$education,
+      geom="histogram",
+      main = "Education", 
+      xlab = "Educational level", 
+      fill=I("dark blue"),
+      col=I("green"))
+
+#Group the data in equal groups
+install.packages("Hmisc")
+library(Hmisc)
+wave3data$ForestCoverGroups <- cut2(wave3data$forest.ha, g=4)
+count(wave3data, ForestCoverGroups)
+
+#new group names
+levels(wave3data$ForestCoverGroups)<-c("very low cover", "low cover", "medium cover", "high cover" )
+
+
+#Plot the data
+par(mar=c(7,5,1,1))
+# Plotting DDS for the 4 groups
+boxplot(mhdds9 ~ ForestCoverGroups, data=wave3data,xlab = '',
+        ylab = 'Dietary diversity score', las=2, col = 2:4)
+
+#Add wealth groups
+wave3data$wealth.score<-as.factor(wave3data$wealth.score)
+
+#ggplot
+ggplot(wave3data, aes(x=ForestCoverGroups, y=mhdds9, fill=wealth.score)) + geom_boxplot() + facet_grid(~wealth.score)+ scale_fill_brewer(palette = "RdYlGn")
+#ggplot2 - combined
+ggplot(wave3data, aes(x=ForestCoverGroups, y=mhdds9, fill=wealth.score)) + geom_boxplot() + scale_fill_brewer(palette = "RdYlGn")+xlab('Forest cover')+ylab('Dietary diversity score')
