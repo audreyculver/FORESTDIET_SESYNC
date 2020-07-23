d0182c63c717d5f40d922b15124de749f024833c
qdread <qread@sesync.org>
2020-07-23 18:36:36 +0000

    Create copy of interactive map script and correct some typos

---
+++ qdr_leafletmap.R
@@ -0,0 +1,81 @@
+library(raster)
+library(sf)
+library(leaflet)
+library(sp)
+library(dplyr)
+
+data <- read.csv("wave3data.csv")
+
+####add colors####
+vec_breaksC <- c(0,2,4, Inf)
+vec_colors <- c("#de2d26", "#fa9fb5", "#c51b8a")
+
+# Pull out coordinates of each cluster from data
+
+# slice function to take first row from each cluster group
+# dplyr select to distinguish from select fn in raster package
+
+cluster_coords <- data %>%
+  group_by(cluster.id) %>%
+  slice(1) %>%
+  dplyr::select(lat, long, forest.ha)
+
+
+####creating cluster dataset####
+
+ClusterData <- data %>%
+  group_by(cluster.id) %>%
+  dplyr::summarise(MeanWealth=mean(wealth.score), MeanMarketDistance=mean(dist.market), countclusters=n(),
+                   MeanDDS=mean(mhdds9), ForestCover=mean(forest.ha))
+
+
+ClusterData$WealthGroup <- ClusterData$MeanWealth
+ClusterData$color <- NA
+for (i in 1: length(ClusterData$WealthGroup)) {
+  ClusterData$color[i] <- vec_colors[min(which(vec_breaksC > ClusterData$WealthGroup[i])) -1]
+}
+
+##Add forest cover groups
+#Group the data in equal groups
+library(Hmisc)
+ClusterData$ForestCoverGroups <- cut2(ClusterData$ForestCover, g=4)
+count(ClusterData, ForestCoverGroups)
+
+
+#new group names
+levels(ClusterData$ForestCoverGroups)<-c("very low cover", "low cover", "medium cover", "high cover" )
+
+##Add DDS groups
+#Group the data in equal groups
+ClusterData$mDDSgroups <- cut2(ClusterData$MeanDDS, g=4)
+count(ClusterData, mDDSgroups)
+
+#new group names
+levels(ClusterData$mDDSgroups)<-c("very low DDS", "low DDS", "medium DDS", "high DDS" )
+
+
+####content of markers#####
+content <- paste(sep="", "CLUSTER INFORMATION", "<br>", "Average wealth score: ", round(ClusterData$MeanWealth,digits = 1), "<br>", 
+                 "Average distance to market (km): ", round(ClusterData$MeanMarketDistance, digits = 1), "<br>",
+                 "Number of households: ", ClusterData$countclusters, "<br>",
+                 "Average dietary diversity score: ", round(ClusterData$MeanDDS,digits = 1), "<br>",
+                 "Forest Cover (ha): ", round(ClusterData$ForestCover, digits = 1))
+
+
+
+
+#####add markers####
+leaflet() %>%
+  addTiles() %>%
+  setView(lng = 35, lat = -5, 
+          zoom = 6) %>%
+  addCircleMarkers(lng = cluster_coords$long, 
+             lat = cluster_coords$lat, 
+             label = round(cluster_coords$forest.ha),
+             popup = content,
+             clusterOptions = markerClusterOptions(),
+             color = ClusterData$color)
+
+#^^this code will generate map with icons that cluster as you zoom out, pop up with cluster info
+
+
